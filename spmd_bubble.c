cfa4d41856503f88aaa8bb52723d13c8a24c499d
Sylvio Correa <sylvio.acad@acad.pucrs.br>
2019-06-15 22:40:49 -0300

    Another commit.

--- spmd_bubble.c
+++ spmd_bubble.c
@@ -59,7 +59,7 @@ void print_arr(int *arr, int size) {
     printf("\n");
 }
 
-void main() {
+void main(int argc, char **argv) {
     
     MPI_Init(&argc , &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
